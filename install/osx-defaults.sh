86a3f7ba0c84bc5931a4247f185dfa008dbb7b5f
Bill Mers <bill@7x7labs.com>
2013-09-27 00:05:30 -0700

    Added OSX setting to suppress iCloud save dialogs.

--- install/osx-defaults.sh
+++ install/osx-defaults.sh
@@ -3,6 +3,8 @@ defaults write com.apple.frameworks.diskimages skip-verify true
 defaults write com.apple.screencapture location ~/Pictures/Screenshots
 defaults write com.apple.Dock showhidden -bool YES
 
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
 chflags nohidden ~/Library/
 
 killall SystemUIServer
