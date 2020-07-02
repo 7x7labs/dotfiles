d805d379d7e33b403f1148f13de5439e31d9b5fb
Bill Mers <bill@lookdeep.ai>
2020-07-02 13:26:33 -0700

    Updated ruby config.

--- zsh/modules/ruby.zsh
+++ zsh/modules/ruby.zsh
@@ -2,6 +2,7 @@ export RAILS_ENV="development"
 
 alias b="bundle"
 alias be="bundle exec"
-alias fs="clear && bundle exec foreman start"
+alias fs="clear && foreman start"
 
+export PATH=$HOME/.rbenv/bin:$PATH
 eval "$(rbenv init -)"
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,7 +5,7 @@ source $ZSH/bindkeys.zsh
 source $ZSH/completion.zsh
 source $ZSH/options.zsh
 source $ZSH/python.zsh
-# source $ZSH/ruby.zsh
+source $ZSH/ruby.zsh
 source $ZSH/terminal.zsh
 source $ZSH/highlighting.zsh
 
